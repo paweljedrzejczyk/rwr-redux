c6e5d11ff2a71c0e6dbdae901e1729afda78fa1d
Kacper Golinski <kacper.golinski@netguru.co>
2016-02-15 19:42:03 +0100

    create redux_store and redux_container view helpers

--- lib/react_webpack_rails/redux_integration/view_helpers.rb
+++ lib/react_webpack_rails/redux_integration/view_helpers.rb
@@ -1,6 +1,22 @@
 module ReactWebpackRails
   module ReduxIntegration
     module ViewHelpers
+      def redux_store(name, raw_props = {}, options = {})
+        react_element('redux-store', { name: name, props: serialize_props(raw_props) }, options)
+      end
+
+      def redux_container(name, options = {})
+        store_name = options.delete(:store_name)
+        react_element('redux-container', { name: name, storeName: store_name }, options)
+      end
+
+      private
+
+      def serialize_props(raw_props)
+        props = raw_props.as_json
+        return props unless Rails.application.config.react.camelize_props
+        ReactWebpackRails::Services::CamelizeKeys.call(props)
+      end
     end
   end
 end
