5c47a54df907e95aa1c664b84d3ac3ae870a8b75
Kacper Golinski <kacper.golinski@netguru.co>
2016-02-26 15:27:41 +0100

    update feature test
    Testing devtools is not necessary.

--- spec/rails4_dummy_app/spec/features/user_can_see_counter_component_spec.rb
+++ spec/rails4_dummy_app/spec/features/user_can_see_counter_component_spec.rb
@@ -4,18 +4,21 @@ feature 'user_can_see_counter_component', js: true do
   before { visit counter_path }
 
   it 'renders counter app with the initial state from controller' do
-    expect(page).to have_content('Clicked: 17 times')
+    expect(page).to have_content('Clicked: 17 times', count: 2)
   end
 
   it 'renders devTools with the initial state from controller' do
-    expect(page).to have_content('state:{} 1 key')
     expect(page).to have_content('counter:17')
   end
 
   it 'synchronizes state between counter app and devTools' do
-    find('button', text: '+').click
+    within('#first-counter-app') do
+      find('button', text: '+').click
+      expect(page).to have_content('Clicked: 18 times')
+    end
 
-    expect(page).to have_content('Clicked: 18 times')
-    expect(page).to have_content('counter:18')
+    within('#second-counter-app') do
+      expect(page).to have_content('Clicked: 18 times')
+    end
   end
 end
