604074e596694862ff7a1affce65da27e52ff4a1
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-25 11:07:11 +0100

    spec refactor

--- spec/rails4_dummy_app/spec/helpers/react_webpack_rails/redux_integration/view_helpers_spec.rb
+++ spec/rails4_dummy_app/spec/helpers/react_webpack_rails/redux_integration/view_helpers_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 describe ReactWebpackRails::ReduxIntegration::ViewHelpers, type: :helper do
+  let(:node_runner_class) { ReactWebpackRails::NodeIntegrationRunner }
+
   describe '#redux_store' do
+    let(:store_args) { ['redux-store', { name: 'StoreName', props: { 'foo' => 'bar' } }] }
+
     it { expect(helper).to respond_to(:redux_store) }
 
     it 'calls #react_element with proper params' do
-      expect(helper)
-        .to receive(:react_element)
-        .with('redux-store', { name: 'StoreName', props: { 'foo_bar' => 'baz' } }, {})
-        .once
+      expect(helper).to receive(:react_element).with(*store_args, {}).once
 
-      helper.redux_store('StoreName', foo_bar: 'baz')
+      helper.redux_store('StoreName', foo: 'bar')
     end
 
     context 'when props are not passed' do
@@ -34,10 +35,10 @@ describe ReactWebpackRails::ReduxIntegration::ViewHelpers, type: :helper do
       it 'calls CamelizeKeys service' do
         expect(ReactWebpackRails::Services::CamelizeKeys)
           .to receive(:call)
-          .with('foo_bar' => 'baz')
+          .with('foo' => 'bar')
           .once
 
-        helper.redux_store('StoreName', foo_bar: 'baz')
+        helper.redux_store('StoreName', foo: 'bar')
       end
     end
 
@@ -45,26 +46,19 @@ describe ReactWebpackRails::ReduxIntegration::ViewHelpers, type: :helper do
       let(:node_runner) { instance_double('ReactWebpackRails::NodeIntegrationRunner') }
 
       before do
-        allow(ReactWebpackRails::NodeIntegrationRunner).to receive(:new) { node_runner }
+        allow(node_runner_class).to receive(:new) { node_runner }
         allow(node_runner).to receive(:run)
       end
 
       it 'calls NodeIntegrationRunner instance with proper arguments' do
-        expect(ReactWebpackRails::NodeIntegrationRunner).to receive(:new)
-          .with('redux-store', name: 'StoreName', props: { 'foo' => 'bar' })
-          .and_return(node_runner)
-          .once
-
+        expect(node_runner_class).to receive(:new).with(*store_args).and_return(node_runner).once
         expect(node_runner).to receive(:run).once
 
         helper.redux_store('StoreName', { foo: 'bar' }, server_side: true)
       end
 
       it 'calls #react_element with proper params' do
-        expect(helper)
-          .to receive(:react_element)
-          .with('redux-store', { name: 'StoreName', props: { 'foo' => 'bar' } }, {})
-          .once
+        expect(helper).to receive(:react_element).with(*store_args, {}).once
 
         helper.redux_store('StoreName', { foo: 'bar' }, server_side: true)
       end
@@ -72,13 +66,14 @@ describe ReactWebpackRails::ReduxIntegration::ViewHelpers, type: :helper do
   end
 
   describe '#redux_container' do
+    let(:container_arguments) do
+      ['redux-container', { name: 'ContainerName', storeName: 'StoreName' }]
+    end
+
     it { expect(helper).to respond_to(:redux_container) }
 
     it 'calls #react_element with proper params' do
-      expect(helper)
-        .to receive(:react_element)
-        .with('redux-container', { name: 'ContainerName', storeName: 'StoreName' }, {})
-        .once
+      expect(helper).to receive(:react_element).with(*container_arguments, {}).once
 
       helper.redux_container('ContainerName', store_name: 'StoreName')
     end
@@ -95,27 +90,23 @@ describe ReactWebpackRails::ReduxIntegration::ViewHelpers, type: :helper do
     end
 
     context 'when server_side is true' do
-      let(:node_runner) { instance_double('ReactWebpackRails::NodeIntegrationRunner') }
+      let(:node_runner) { instance_double('node_runner_class') }
       let(:result_string) { '<div>result</div>' }
-      let(:arguments) { ['redux-container', { name: 'ContainerName', storeName: nil }, {}] }
 
       before do
-        allow(ReactWebpackRails::NodeIntegrationRunner).to receive(:new) { node_runner }
+        allow(node_runner_class).to receive(:new) { node_runner }
         allow(node_runner).to receive(:run)
       end
 
       it 'NodeIntegrationRunner instance and #react_element with proper arguments' do
-        expect(ReactWebpackRails::NodeIntegrationRunner).to receive(:new)
-          .with('redux-container', name: 'ContainerName', storeName: nil)
-          .and_return(node_runner)
+        expect(node_runner_class).to receive(:new).with(*container_arguments) { node_runner }
+        expect(node_runner).to receive(:run) { result_string }
 
-        expect(node_runner).to receive(:run).and_return(result_string)
-
-        expect(helper).to receive(:react_element).with(*arguments) do |*args, &block|
+        expect(helper).to receive(:react_element).with(*container_arguments, {}) do |*args, &block|
           expect(block.call).to eq(result_string.html_safe)
         end
 
-        helper.redux_container('ContainerName', server_side: true)
+        helper.redux_container('ContainerName', store_name: 'StoreName', server_side: true)
       end
     end
   end
