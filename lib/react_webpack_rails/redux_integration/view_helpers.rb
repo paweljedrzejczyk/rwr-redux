f8750488775f98b7d19a3bfe77725171e04f2498
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-01 14:32:20 +0200

    handle 404 and redirect with server side  redux router

--- lib/react_webpack_rails/redux_integration/view_helpers.rb
+++ lib/react_webpack_rails/redux_integration/view_helpers.rb
@@ -27,18 +27,15 @@ module ReactWebpackRails
         end
       end
 
-
       def redux_router(name, options = {})
         store_name = options.delete(:store_name)
         payload = { name: name, storeName: store_name }
 
         if server_side(options.delete(:server_side))
-          server_payload = payload.merge(path: request.path)
-          result_string = NodeIntegrationRunner.new('redux-router', server_payload).run
-          result = JSON.parse(result_string)
+          result = server_side_router(payload)
 
           react_element('redux-router', payload, options) do
-            result['body'].html_safe
+            result
           end
         else
           react_element('redux-router', payload, options)
@@ -51,6 +48,26 @@ module ReactWebpackRails
         props = raw_props.as_json
         return props unless Rails.application.config.react.camelize_props
         ReactWebpackRails::Services::CamelizeKeys.call(props)
+      end
+
+      def server_side_router(payload)
+        path = request.path
+        server_payload = payload.merge(path: path)
+        result_string = NodeIntegrationRunner.new('redux-router', server_payload).run
+        result = JSON.parse(result_string)
+
+        case result['code']
+        when 200
+          result['body'].html_safe
+        when 302
+          controller.redirect_to(result['redirectUri'])
+        else
+          fail ActionController::RoutingError, routing_error(payload[:name], path)
+        end
+      end
+
+      def routing_error(name, path)
+        "ReactWebpackRails::ReduxIntegration: No route found in #{name} router for #{path}."
       end
     end
   end
