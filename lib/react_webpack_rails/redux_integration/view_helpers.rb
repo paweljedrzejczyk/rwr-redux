459de5ed9ef6b9fbb107bffd9a8389afdd0dee16
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-01 14:18:42 +0200

    create redux_router view helper

--- lib/react_webpack_rails/redux_integration/view_helpers.rb
+++ lib/react_webpack_rails/redux_integration/view_helpers.rb
@@ -13,14 +13,35 @@ module ReactWebpackRails
 
       def redux_container(name, options = {})
         store_name = options.delete(:store_name)
+        payload = { name: name, storeName: store_name }
 
         if server_side(options.delete(:server_side))
-          result_string = NodeIntegrationRunner.new('redux-container', name: name, storeName: store_name).run
-          react_element('redux-container', { name: name, storeName: store_name }, options) do
-            result_string.html_safe
+          result_string = NodeIntegrationRunner.new('redux-container', payload).run
+          result = JSON.parse(result_string)
+
+          react_element('redux-container', payload, options) do
+            result['body'].html_safe
           end
         else
-          react_element('redux-container', { name: name, storeName: store_name }, options)
+          react_element('redux-container', payload, options)
+        end
+      end
+
+
+      def redux_router(name, options = {})
+        store_name = options.delete(:store_name)
+        payload = { name: name, storeName: store_name }
+
+        if server_side(options.delete(:server_side))
+          server_payload = payload.merge(path: request.path)
+          result_string = NodeIntegrationRunner.new('redux-router', server_payload).run
+          result = JSON.parse(result_string)
+
+          react_element('redux-router', payload, options) do
+            result['body'].html_safe
+          end
+        else
+          react_element('redux-router', payload, options)
         end
       end
 
