e5d6751df3dad8918d6b183872ab9a4290b06af6
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-01 11:26:35 +0200

    create integrations/redux-store.js

---
+++ js/src/integrations/redux-store.js
@@ -0,0 +1,46 @@
+import { isFunction, isReduxStore } from './utils/validators';
+
+class ReduxStore {
+  constructor() {
+    this.registeredStores = {};
+    this.mountedStores = {};
+    this.defaultStore = null;
+  }
+
+  registerStore(name, store) {
+    isFunction(store, `Error when registering '${name}' store: must be a function.`);
+    this.registeredStores[name] = store;
+  }
+
+  mountStore(name, props) {
+    const store = this.registeredStores[name];
+    isFunction(store, `Error when mounting '${name}' store: must be a function.`);
+
+    const storeObject = store(props);
+    isReduxStore(storeObject, `Error when mounting '${name}' store: must be a valid Redux store.`);
+    this.mountedStores[name] = storeObject;
+    this.defaultStore = storeObject;
+  }
+
+  getStore(name) {
+    if (name) {
+      return this.mountedStores[name];
+    }
+
+    return this.defaultStore;
+  }
+
+  get storeIntegrationWrapper() {
+    return {
+      mount: function _mount(_, payload) {
+        this.mountStore(payload.name, payload.props);
+      }.bind(this),
+
+      nodeRun: function _mount(payload) {
+        this.mountStore(payload.name, payload.props);
+      }.bind(this),
+    };
+  }
+}
+
+export default new ReduxStore;
