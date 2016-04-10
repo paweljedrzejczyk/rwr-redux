400e27cb1785c3f51b4d56e5afde1c3d04397721
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-10 17:32:14 +0200

    create redux_container and redux_router feature tests

--- spec/rails4_dummy_app/app/controllers/pages_controller.rb
+++ spec/rails4_dummy_app/app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  def counter
+  def redux_container
     @counter_initial_value = 17
+  end
+
+  def redux_router
+    @counter_initial_value = 91
   end
 end
--- spec/rails4_dummy_app/config/routes.rb
+++ spec/rails4_dummy_app/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get 'counter', to: 'pages#counter'
+  get 'redux_container', to: 'pages#redux_container'
+  get 'redux_router', to: 'pages#redux_router'
 end
